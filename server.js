a883d0b40938c33aaa1ea353a599b69cdfaabd20
Anderson Zanichelli <andersonzanichelli@gmail.com>
2015-05-14 05:35:12 +0000

    Adicionado busca por email

--- server.js
+++ server.js
@@ -3,21 +3,31 @@ var mongodb = require('mongodb');
 
 var server = restify.createServer();
 var uri = 'mongodb://broker:BrokerServer2015@ds063180.mongolab.com:63180/brokerserver';
+var port = process.env.PORT || 9000;
 
 var brokerserver = {};
 
 brokerserver.types = function(req, res, next){
     console.error('Client connected at ' + new Date());
-    brokerserver.consulta('types', res);
+    brokerserver.consulta('types', res, {});
     next();
 };
 
-brokerserver.consulta = function(collections, res) {
+brokerserver.email = function(req, res, next) {
+    brokerserver.consulta('users', res, {email: req.params['email']});
+    next();
+};
+
+brokerserver.consulta = function(collections, res, filter) {
     mongodb.MongoClient.connect(uri, function(err, db) {
         if(err) throw err;
 
         var collection = db.collection(collections);
-        collection.find().toArray(function(err, docs) {
+        collection.find(filter).toArray(function(err, docs) {
+            if(err) {
+                res.json(err);
+                return;
+            }
             res.json(docs);
         });
     });
@@ -30,7 +40,8 @@ server.use(function(req, res, next) {
 });
 
 server.get('/types', brokerserver.types);
+server.get('/email/:email', brokerserver.email);
 
-server.listen(9000, function() {
-  console.log('%s listening at %s', server.name, server.url);
+server.listen(port, function() {
+  console.log('%s listening at port %s', 'BrokerServer', port);
 });
