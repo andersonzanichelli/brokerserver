b464d3d80c257ee57b92f82c3eb0f51c1b441fd4
Anderson Zanichelli <anderzanic@gmail.com>
2015-05-30 14:58:01 -0300

    Teste para salvar usuário.

--- server.js
+++ server.js
@@ -8,28 +8,73 @@ var port = process.env.PORT || 9000;
 var brokerserver = {};
 
 brokerserver.types = function(req, res, next){
-    console.error('Client connected at ' + new Date());
-    brokerserver.consulta('types', res, {});
+    var params = {
+        "operation": brokerserver.find,
+        "collection": 'types',
+        "filter": {},
+        "response": res,
+        "callback": undefined
+    };
+    brokerserver.dbOperations(params);
     next();
 };
 
 brokerserver.email = function(req, res, next) {
-    brokerserver.consulta('users', res, {email: req.params['email']});
+    var params = {
+        "operation": brokerserver.find,
+        "collection": 'users',
+        "filter": {email: req.params['email']},
+        "response": res,
+        "callback": undefined
+    };
+    brokerserver.dbOperations(params);
     next();
 };
 
-brokerserver.consulta = function(collections, res, filter) {
+brokerserver.signup = function(req, res, next) {
+    var params = {
+        "operation": brokerserver.find,
+        "collection": 'users',
+        "filter": {email: req.params['email']},
+        "response": res,
+        "callback": brokerserver.insert,
+        "request": req
+    };
+
+    brokerserver.dbOperations(params);
+    next();
+};
+
+brokerserver.insert = function(params){
+    console.log(params.request);
+    var document = params.db.collection.insert(params.request.params);
+    console.log(document);
+
+    params.response.json({"_id": 1});
+};
+
+brokerserver.find = function(params) {
+    var collection = params.db.collection(params.collection);
+    collection.find(params.filter).toArray(function(err, docs) {
+        if(err) {
+            params.response.json(err);
+            return;
+        }
+
+        if(params.callback){
+            params.docs = docs;
+            params.callback(params);
+        } else
+            params.response.json(docs);
+    });
+};
+
+brokerserver.dbOperations = function(params) {
     mongodb.MongoClient.connect(uri, function(err, db) {
         if(err) throw err;
 
-        var collection = db.collection(collections);
-        collection.find(filter).toArray(function(err, docs) {
-            if(err) {
-                res.json(err);
-                return;
-            }
-            res.json(docs);
-        });
+        params.db = db;
+        params.operation(params);
     });
 };
 
@@ -41,6 +86,7 @@ server.use(function(req, res, next) {
 
 server.get('/types', brokerserver.types);
 server.get('/email/:email', brokerserver.email);
+server.post('/signup', brokerserver.signup);
 
 server.listen(port, function() {
   console.log('%s listening at server port %s', 'BrokerServer', port);
